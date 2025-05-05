6c79104db75dc8e9e4d9be55445f8c19fcd66175
vijay rajesh <ewe111.vijay@gmail.com>
2025-05-05 15:15:55 +0500

    nm

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ require('dotenv').config();
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: 'https://backend-internship-portal.vercel.app', 
+  origin: 'http://localhost:3000', 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
