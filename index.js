e6e018377657cdcf665f479716b04767a1cfe7e6
vijay rajesh <ewe111.vijay@gmail.com>
2025-05-20 11:54:52 +0500

    ..

--- index.js
+++ index.js
@@ -1526,6 +1526,10 @@ app.get("/health", (req, res) => {
   });
 });
 
-
+// // Start the server
+// const PORT = process.env.PORT || 8000;
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
 
 module.exports = app;
