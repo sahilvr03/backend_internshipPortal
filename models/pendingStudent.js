089a44b2ac539e5717bfcf39150d12ef9163c2a3
vijay rajesh <ewe111.vijay@gmail.com>
2025-06-25 15:50:25 +0500

    ,,

--- models/pendingStudent.js
+++ models/pendingStudent.js
@@ -1,18 +1,19 @@
 const mongoose = require("mongoose");
 
+// Yeh schema Student Schema ka carbon copy hoga
 const pendingStudentSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true }, // Hashed password
-
+  
   // Baaqi ki saari profile information
   contactNumber: String,
   program: String,
   university: String,
   graduationYear: Number,
   bio: String,
-
+  
   // Default settings
   notificationSettings: {
     emailNotifications: { type: Boolean, default: true },
